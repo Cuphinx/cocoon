f0fe3f3a0b8e3026b24f7146ed220322f758f452
John "codefu" McDole <codefu@google.com>
2026-02-24 17:08:17 -0800

    Fix pub workspace (#4955)
    fix missing workspace in cloud build
    - app_dart and auto_submit + all deps in the /app folder.
    - update build script not to download tools a second time (the image
      should be updated weekly - todo)
    
    ---------
    
    Co-authored-by: Dmitry Grand <dmgr@google.com>

--- cloud_build/dashboard_build.sh
+++ cloud_build/dashboard_build.sh
@@ -5,12 +5,10 @@
 
 # Build flutter build dashboard.
 
-pushd dashboard > /dev/null
+pushd app/dashboard > /dev/null
 set -e
 rm -rf build
-flutter channel stable
-flutter upgrade
-flutter doctor
+flutter doctor -v
 flutter pub get
 flutter config --enable-web
 flutter build web --source-maps
